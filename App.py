d0910436f8b066542f0d86fa1b88f86ec9fdce1d
yashvasudeva1 <117675995+yashvasudeva1@users.noreply.github.com>
2025-09-03 12:50:40 +0530

    Update App.py

--- App.py
+++ App.py
@@ -6,10 +6,9 @@ st.write("This app helps you in making your dataset cleaner, outlier free and re
 file=st.file_uploader("Upload Your CSV File")
 if file is not None:
     df = pd.read_csv(file)
-df_num=df.select_dtypes(include='number')
-column=df.columns
-st.write(df)
-tab0,tab1,tab2,tab3,tab4=st.tabs(["General Analysis","Visual Representation","Facts", "Outlier Abalysis","Make Predictions"])
-if tab0:
-  st.write(df.describe())
+    column=df.columns
+    st.write(df)
+    tab0,tab1,tab2,tab3,tab4=st.tabs(["General Analysis","Visual Representation","Facts", "Outlier Abalysis","Make Predictions"])
+    if tab0:
+      st.write(df.describe())
   
