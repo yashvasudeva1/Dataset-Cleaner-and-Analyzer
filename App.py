bbc3d6ead7f66f42311e5beb705e87f6276f113d
yashvasudeva1 <117675995+yashvasudeva1@users.noreply.github.com>
2025-09-03 12:38:23 +0530

    Update App.py

--- App.py
+++ App.py
@@ -5,6 +5,7 @@ st.title("Dataset Cleaner and Analyser")
 st.write("This app helps you in making your dataset cleaner, outlier free and ready for training")
 file=st.file_uploader("Upload Your CSV File")
 df=pd.read_csv(file)
+column=df.columns
 st.write(df.describe())
 tab1,tab2,tab3,tab4=st.tabs(["Visual Representation","General Analysis", "Outlier Abalysis","Make Predictions"])
 if tab1:
