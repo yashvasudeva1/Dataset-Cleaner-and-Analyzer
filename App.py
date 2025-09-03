9db24552d9166984c6a65c8045d720f12d6a5ac1
yashvasudeva1 <117675995+yashvasudeva1@users.noreply.github.com>
2025-09-03 12:43:10 +0530

    Update App.py

--- App.py
+++ App.py
@@ -5,9 +5,10 @@ st.title("Dataset Cleaner and Analyser")
 st.write("This app helps you in making your dataset cleaner, outlier free and ready for training")
 file=st.file_uploader("Upload Your CSV File")
 df=pd.read_csv(file)
+df_num=df.select_dtypes(include='number')
 column=df.columns
-st.write(df.describe())
-tab0,tab1,tab2,tab3,tab4=st.tabs(["Home","Visual Representation","General Analysis", "Outlier Abalysis","Make Predictions"])
-if tab1:
-  for i in column:
-    st.line_chart(df['carat'])
+st.write(df)
+tab0,tab1,tab2,tab3,tab4=st.tabs(["General Analysis","Visual Representation","Facts", "Outlier Abalysis","Make Predictions"])
+if tab0:
+  st.write(df.describe())
+  
