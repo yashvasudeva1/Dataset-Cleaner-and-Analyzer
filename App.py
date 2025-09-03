46183419ccfd4d409de76d53444e4ee7055982ce
yashvasudeva1 <117675995+yashvasudeva1@users.noreply.github.com>
2025-09-03 14:17:41 +0530

    Update App.py

--- App.py
+++ App.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns  # Make sure to import seaborn
 
 st.title("Dataset Cleaner and Analyser")
 st.write("This app helps you in making your dataset cleaner, outlier free and ready for training")
@@ -13,28 +14,19 @@ def load_data(uploaded_file):
 file = st.file_uploader("Upload Your CSV File")
 if file is not None:
     df = load_data(file)
-    df_num = df.select_dtypes(include=[np.number])
-    columns = df.columns
     st.write("Preview of your dataset:")
     st.dataframe(df, use_container_width=True)
-
-    tab0, tab1, tab2, tab3, tab4 , tab5 = st.tabs(
-        ["General Analysis", "Visual Representation", "Facts", "Outlier Analysis", "Make Predictions","Check the Type of Distribution"]
-    )
-    data = np.random.normal(0, 1, size=100)  # Use your real column here
-
-    fig, ax = plt.subplots()
-    ax.hist(data, bins=20, color='skyblue', edgecolor='black')
-    ax.set_title('Histogram Example')
-    ax.set_xlabel('Value')
-    ax.set_ylabel('Frequency')
     
-    st.pyplot(fig)
+    tab0, tab1, tab2, tab3, tab4, tab5 = st.tabs(
+        ["General Analysis", "Visual Representation", "Facts", "Outlier Analysis", "Make Predictions", "Check the Type of Distribution"]
+    )
+    
     with tab0:
         st.write(df.describe())
 
     with tab1:
         st.line_chart(df.select_dtypes(include='number'), height=250, use_container_width=True)
+
     with tab3:
         columns = df.select_dtypes(include=[np.number]).columns
         outlier_report = []
@@ -48,6 +40,7 @@ if file is not None:
         outliers = pd.DataFrame(outlier_report)
         st.write(outliers)
         st.button("Remove the Outliers")
+    
     with tab5:
         numeric_cols = df.select_dtypes(include=np.number).columns
         for col in numeric_cols:
