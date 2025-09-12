faef6bb09b862a644ce76bdb645a1c0ca3da4588
yashvasudeva1 <117675995+yashvasudeva1@users.noreply.github.com>
2025-09-12 19:10:10 +0530

    Update App.py

--- App.py
+++ App.py
@@ -112,22 +112,24 @@ if file is not None:
                 ]
             )
             target_column = st.selectbox("Select the Target Column:", [columns,'None'])
-            if model_selection=='Linear Regression' and target_column != 'None':
-                column=df.columns
-                for i in column:
-                    q1,q3=df[i].quantile([0.25,0.75])
-                    iqr=q3-q1
-                    lower_bound=q1-(1.5*iqr)
-                    upper_bound=q3+(1.5*iqr)
-                    df_cleaned=df[(df[i]>=lower_bound) & (df[i]<=upper_bound)]
-                x=df_cleaned.drop(target_column,axis=1)
-                y=df_cleaned[target_column]
-                x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=42)
-                scalar=StandardScaler()
-                x_train=scalar.fit_transform(x_train)
-                x_test=scalar.transform(x_test)
-                model=LinearRegression()
-                model.fit(x_train,y_train)
+            if model_selection == 'Linear Regression' and target_column != 'None':
+            df_cleaned = df.copy()
+            for col in df_cleaned.columns:
+                if col == target_column:
+                    continue
+                q1, q3 = df_cleaned[col].quantile([0.25, 0.75])
+                iqr = q3 - q1
+                lower_bound = q1 - 1.5 * iqr
+                upper_bound = q3 + 1.5 * iqr
+                df_cleaned = df_cleaned[(df_cleaned[col] >= lower_bound) & (df_cleaned[col] <= upper_bound)]
+            x = df_cleaned.drop(target_column, axis=1)
+            y = df_cleaned[target_column]
+            x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+            scaler = StandardScaler()
+            x_train = scaler.fit_transform(x_train)
+            x_test = scaler.transform(x_test)
+            model = LinearRegression()
+            model.fit(x_train, y_train)
                 
         elif dataset_choice == "Classification Type":
             model_selection = st.selectbox(
