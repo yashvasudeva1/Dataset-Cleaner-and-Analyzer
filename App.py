fc23dbc6979a549b6f80098d37bfdc09c8e4ffe3
yashvasudeva1 <117675995+yashvasudeva1@users.noreply.github.com>
2025-09-13 23:05:38 +0530

    Update App.py

--- App.py
+++ App.py
@@ -293,7 +293,7 @@ if file is not None:
                 ridge=Ridge()
                 param=np.arange(0.000000001,101,1)
                 parameters={'alpha':param}
-                ridgecv=GridSearchCV(ridge,parameters,scoring='neg_mean_squared_error',cv=10)
+                ridgecv=GridSearchCV(ridge,parameters,scoring='neg_root_mean_squared_error',cv=10)
                 ridgecv.fit(x_train,y_train)
                 y_pred=ridgecv.predict(x_test)
                 st.success("""Model Trained Successfully   
