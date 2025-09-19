d45d973414d04f5c1e26d9ad5d6b26ba614912a5
yashvasudeva1 <117675995+yashvasudeva1@users.noreply.github.com>
2025-09-19 17:43:59 +0530

    Update App.py

--- App.py
+++ App.py
@@ -34,11 +34,11 @@ def shapiro_safe(x):
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", message=".*computed p-value may not be accurate.*")
         return stats.shapiro(x)
-logo_path = "image.jpg"  # Use your image file
+logo_path = "transparent.png"  # Use your image file
 col1, col2 = st.columns([1, 6])
 
 with col1:
-    st.image(logo_path, width=40)  # Adjust width as needed
+    st.image(logo_path, width=400)  # Adjust width as needed
 
 with col2:
     st.markdown("<h1 style='margin-bottom:0;'>Dataset Cleaner and Analyser</h1>", unsafe_allow_html=True)
