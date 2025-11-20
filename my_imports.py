7d05372a82678394092bab5b7739b3fe402ab64b
Yash Vasudeva <117675995+yashvasudeva1@users.noreply.github.com>
2025-11-20 21:14:08 +0530

    Add files via upload

---
+++ my_imports.py
@@ -0,0 +1,25 @@
+def import_all_libs():
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    from scipy import stats
+    import warnings
+    import io
+    import requests
+    import altair as alt
+    from sklearn.preprocessing import LabelEncoder
+    from sklearn.utils.multiclass import type_of_target
+    from sklearn.svm import SVR, SVC
+    from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet, LogisticRegression
+    from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
+    from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor, RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier
+    from sklearn.neighbors import KNeighborsRegressor, KNeighborsClassifier
+    from sklearn.naive_bayes import GaussianNB
+    from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+    from lightgbm import LGBMClassifier
+    from sklearn.neural_network import MLPClassifier
+    from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+    from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+    from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+    from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
