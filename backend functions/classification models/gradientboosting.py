e667f87b9ab1afaa91a8441a3cee32aa3300fab7
Yash Vasudeva <117675995+yashvasudeva1@users.noreply.github.com>
2025-11-21 00:51:54 +0530

    Update gradientboosting.py

--- backend functions/classification models/gradientboosting.py
+++ backend functions/classification models/gradientboosting.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append("backend functions/functionalities")
+from importlibraries import *
 def tune_gradient_boosting(x_train, y_train, x_test, y_test):
     model = GradientBoostingClassifier()
     params = {'n_estimators': [100, 200], 'learning_rate': [0.01, 0.1, 0.2], 'max_depth': [3, 5]}
@@ -14,3 +17,4 @@ def tune_gradient_boosting(x_train, y_train, x_test, y_test):
         "F1-score": f1_score(y_test, y_pred, average='weighted')
     }
     return best_model, pd.DataFrame([metrics])
+
