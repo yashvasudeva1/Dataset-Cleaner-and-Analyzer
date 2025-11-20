6ece4f2d423f2f1e1a9655f5f93edcf86122f72a
Yash Vasudeva <117675995+yashvasudeva1@users.noreply.github.com>
2025-11-21 00:52:28 +0530

    Update logisticregression.py

--- backend functions/classification models/logisticregression.py
+++ backend functions/classification models/logisticregression.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append("backend functions/functionalities")
+from importlibraries import *
 def tune_logistic_regression(x_train, y_train, x_test, y_test):
     model = LogisticRegression(max_iter=10000)
     params = {'C': [0.01, 0.1, 1, 10, 100], 'solver': ['liblinear', 'saga']}
@@ -14,3 +17,4 @@ def tune_logistic_regression(x_train, y_train, x_test, y_test):
         "F1-score": f1_score(y_test, y_pred, average='weighted')
     }
     return best_model, pd.DataFrame([metrics])
+
